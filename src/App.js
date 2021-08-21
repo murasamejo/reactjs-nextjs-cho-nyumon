7644df94c646854e53f6469dd04cd5517002d613
Osamu Takiya <takiya@toran.sakura.ne.jp>
2021-08-21 18:59:59 +0900

    feat: 🎸 Redux を使って簡単な挙動を実装した

--- src/App.js
+++ src/App.js
@@ -1,125 +1,52 @@
-import React, { useState } from 'react';
-import './App.css';
+import React from 'react'
+import { connect } from 'react-redux'
+import './App.css'
 
-function App(props) {
-  const [buttonMessage] = useState('Hello, onClick={doAction} World')
-  const [exclamationPointString, setExclamationPointString] = useState('!')
-  const [enteredValueByInputText, setEnteredValueByInputText] = useState('')
-  const [submittedTextByForm, setSubmittedTextByForm] = useState('（初期値）')
+// ステートのマッピング
+const mappingState = (state) => {
+  return state
+}
 
-  const doAction = e => {
-    setExclamationPointString(exclamationPointString + '!')
-  }
-
-  const doSubmit = e => {
-    setSubmittedTextByForm(enteredValueByInputText)
-
-    e.preventDefault()
-  }
-
-  const doChange = e => {
-    setEnteredValueByInputText(e.target.value)
-  }
-
-  const doCheckInputText = e => {
-    alert(`${e.target.value} は長すぎます（最大10文字）`)
-  }
-
-  const sampleListData = [
-    'This is a sample data.',
-    'これはリストのサンプルです。',
-    'リストの各データは配列を用いて格納しています',
-  ]
-
+let App = (props) => {
   return (
     <div>
       <h1>{props.title}</h1>
 
-      <form onSubmit={doSubmit} className="standard-margin">
-        <label>
-          <input type="text" id="input-name" onChange={doChange} name="input-name" />
-        </label>
-
-        <button type="submit">Submit!</button>
-      </form>
-
-      <p>{`今フォームに入力されている値は ${enteredValueByInputText} です。`}</p>
-      <p>{`フォームに入力されて送信された値は ${submittedTextByForm} です。`}</p>
-
-      <hr />
-
-      { (exclamationPointString.length % 2) === 0 ?
-          <p className="exclamation-string-area-in-even">{`${buttonMessage}${exclamationPointString}`}</p>
-        :
-          <p className="exclamation-string-area-in-odd">{`${buttonMessage}${exclamationPointString}`}</p>
-      }
-
-      <button className="button-style" onClick={doAction}>Click Me!</button>
-
-      <hr />
-
-      <h2>リスト表示</h2>
-      <List listTitle="サンプルリスト" listData={sampleListData} />
-
-      <hr />
-      <h2>Children のテスト</h2>
-      <Message>
-        ほげ。
-        ふが。
-        FooFooBarBar
-        foobar。
-      </Message>
-
-      <hr />
-      <MessageLengthChecker maxLength="10" onCheck={doCheckInputText}/>
+      <Message />
+      <Button />
     </div>
   );
 }
 
-function MessageLengthChecker (props) {
-  const doChange = e => {
-    if (e.target.value.length > props.maxLength) {
-      props.onCheck(e)
+// ストアのコネクト (App)
+App = connect()(App)
 
-      e.target.value = e.target.value.substr(0, props.maxLength)
+let Message = (props) => {
+  return(
+    <p>
+      {props.message}: {props.counter}
+    </p>
+  )
+}
+
+// ストアのコネクト (Message)
+Message = connect(mappingState)(Message)
+
+let Button = (props) => {
+  const doAction = e => {
+    if (e.shiftKey) {
+      props.dispatch({ type: 'DECREMENT' })
+    } else {
+      props.dispatch({ type: 'INCREMENT' })
     }
   }
 
-  return (
-    <input type="text" onChange={doChange} />
+  return(
+    <button onClick={doAction}>Click Me!</button>
   )
 }
 
-function List (props) {
-  let [number] = useState(0)
-
-  return (
-    <div>
-      <p className="list-title">{props.listTitle}</p>
-      <ul>
-        { props.listData.map(item => <Item number={number++} value={item} key={number} />) }
-      </ul>
-    </div>
-  );
-}
-
-function Item (props) {
-  return (
-    <li>
-      <span className="item-number">[{props.number}]</span> {props.value}
-    </li>
-  )
-}
-
-function Message (props) {
-  const content = props.children
-  const contents = content.split('。')
-
-  return (
-    <div>
-      <p className="standard-text">Message コンポーネントのノードに含まれる句点の数は {contents.length - 1}個 です。</p>
-    </div>
-  )
-}
+// ストアのコネクト (Button)
+Button = connect()(Button)
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+
+// ストアの初期値
+const stateValue = {
+  counter: 0,
+  message: 'COUNTER',
+}
+
+// レデューサ
+const counter = (state = stateValue, action) => {
+  switch(action.type) {
+    case 'INCREMENT':
+    return {
+      counter: state.counter + 1,
+      message: 'INCREMENT',
+    }
+    case 'DECREMENT':
+    return {
+      counter: state.counter - 1,
+      message: 'DECREMENT',
+    }
+    default:
+      return state
+  }
+}
+
+// ストアの作成
+const store = createStore(counter)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App title="React" />
+    <Provider store={store}>
+      <App title="React" store={store} />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();

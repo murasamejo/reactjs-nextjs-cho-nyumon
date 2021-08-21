615e3697a3cb81fc1674bee749f24006244934d2
Osamu Takiya <takiya@toran.sakura.ne.jp>
2021-08-21 09:51:21 +0900

    feat: 🎸 Rect コンポーネントに border-radius スタイルを当てた

--- src/App.js
+++ src/App.js
@@ -6,12 +6,11 @@ function App(props) {
     <div>
       <h1>{props.title}</h1>
 
-      <Rect x="50" y="50" width="150" height="150" color="cyan" />
-      <Rect x="150" y="100" width="150" height="150" color="magenta" />
-      <Rect x="100" y="150" width="150" height="150" color="black" />
+      <Rect x="50" y="50" width="150" height="150" color="cyan" radius="50" />
+      <Rect x="150" y="100" width="150" height="150" color="magenta" radius="75" />
+      <Rect x="100" y="150" width="150" height="150" color="black" radius="25" />
     </div>
   );
 }
-
 
 export default App;
--- src/Rect.js
+++ src/Rect.js
@@ -6,18 +6,20 @@ function Rect(props) {
   const [width, setWidth] = useState(props.width)
   const [height, setHeight] = useState(props.height)
   const [color, setColor] = useState(props.color)
+  const [radius, setRadius] = useState(props.radius)
   const [style, setStyle] = useState({
     backgroundColor: color,
     position: 'absolute',
     left: x + 'px',
     top: y + 'px',
     width: width + 'px',
-    height: height + 'px'
+    height: height + 'px',
+    borderRadius: radius + 'px'
   })
 
   useEffect(() => {
     // TODO: 必要に応じて追加する
-  }, [setX, setY, setWidth, setHeight, setColor, setStyle])
+  }, [setX, setY, setWidth, setHeight, setColor, setStyle, setRadius])
 
   return (
     <div style={style}></div>

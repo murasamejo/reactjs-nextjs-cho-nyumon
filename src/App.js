689788a7a3511edd53ce4e2ae13c01f4c5cbb753
Osamu Takiya <takiya@toran.sakura.ne.jp>
2021-08-23 17:22:06 +0900

    feat: 🎸 Store を記述した

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import './App.css'
-import Memo from './memo/Memo'
-import AddForm from './memo/AddForm'
-import FindForm from './memo/FindForm'
-import DelForm from './memo/DelForm'
+// import Memo from './memo/Memo'
+// import AddForm from './memo/AddForm'
+// import FindForm from './memo/FindForm'
+// import DelForm from './memo/DelForm'
 
 const App = () => {
   const td = {
@@ -14,20 +14,20 @@ const App = () => {
   return (
     <div>
       <h1>Memo Application with Redux</h1>
-      <AddForm />
+      {/* <AddForm /> */}
 
       <hr />
 
       <table>
         <tbody>
           <tr>
-            <td style={td}><FindForm /></td>
-            <td style={td}><DelForm /></td>
+            {/* <td style={td}><FindForm /></td>
+            <td style={td}><DelForm /></td> */}
           </tr>
         </tbody>
       </table>
 
-      <Memo />
+      {/* <Memo /> */}
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import './index.css'
 import App from './App'
 import MemoStore from './memo/Store'
+// import { memoReducer, addMemo, deleteMemo, findMemo } from './memo/Store'
 import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(

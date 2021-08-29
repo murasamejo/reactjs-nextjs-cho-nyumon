ef3dce433d9399b71644103c7416055bbf605fbc
Osamu Takiya <takiya@toran.sakura.ne.jp>
2021-08-29 17:54:44 +0900

    feat: 🎸 <Head> コンポーネントを使って <title> を出し分けた

--- pages/components/Layout.js
+++ pages/components/Layout.js
@@ -2,6 +2,9 @@ import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
+// このコンポーネントは特別なもの
+import Head from 'next/head'
+
 // props.children は重要
 // この Layout というタグを他で使った際に、その中身（<Layout>（ここ）</Layout>）の部分が代入されている
 // Rails の View でいう yield みたいなもの
@@ -9,6 +12,12 @@ import Footer from '../components/Footer'
 const Layout = props => {
   return (
     <div>
+      <Head>
+        <title>{props.title}</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
       <Header header={props.header} title={props.title} />
       {props.children}
       <Footer footer="Copyright nikukyugamer" />
